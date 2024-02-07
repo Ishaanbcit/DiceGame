2409c64fc89042aa1b3b46abe3733212583b75e3
Pallvi6 <pallvi@blackcapit.com>
2024-02-07 11:53:05 +0530

    first push

---
+++ assets/js/backend.js
@@ -0,0 +1,356 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { getFirestore, collection, addDoc, getDoc, getDocs, doc, setDoc, query, where, onSnapshot, updateDoc, getCountFromServer } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBvyvJIQZjXGm40tJRsur7iAEwrSnQIQ_I",
+    authDomain: "dice-game-294a9.firebaseapp.com",
+    projectId: "dice-game-294a9",
+    storageBucket: "dice-game-294a9.appspot.com",
+    messagingSenderId: "214794687122",
+    appId: "1:214794687122:web:0f00be98fc12de1684cc4b",
+    measurementId: "G-85MYXTVH98"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+//================================== working code ============================================================
+
+//global frontend elements
+//-----form ele
+const codeGenerateBtn = $("#onlineDataModal .modal-footer button");
+const playBtn = $("#onlineDataModal #join-room-form button");
+const codeInputVal = $("#onlineDataModal form #room-code-input");
+const nameInputVal = $("#onlineDataModal .modal-content input#playername");
+const formErrorMsg = $("#onlineDataModal form .error-msg");
+
+//---game dashboard
+const playerName = $("#PlayerName");
+const opponentName = $("#OpponentName");
+const codePlate = $(".room-details #room-code");
+const playerScore = $(".player.player--0 .score");
+const playerCurrentScore = $(".player.player--0 .current-score");
+const opponentScore = $(".player.player--1 .score");
+const opponentCurrentScore = $(".player.player--1 .current-score");
+
+
+
+//global variables
+var roomCode = "";
+var fullRoomID = "";
+var fullDataID = "";
+var fullOpDataID = "";
+var pName = "";
+
+//Generate code button action
+$("#onlineDataModal .modal-footer button").click(function () {
+    generateRoomCode();
+});
+
+//Play button action
+$("#onlineDataModal .modal-content button.play-btn").click(function (e) {
+
+    e.preventDefault();
+
+
+
+    if (nameInputVal.val().trim() == "") {
+        formErrorMsg.text("Please Enter Your name");
+        formErrorMsg.addClass("display");
+    }
+
+    else if (codeInputVal.val().trim() == "") {
+        formErrorMsg.text("Please Enter Room Code");
+        formErrorMsg.addClass("display");
+    }
+
+    else {
+        roomCode = codeInputVal.val().trim();
+        pName = nameInputVal.val().trim();
+        enterPlayer(codeInputVal.val(), nameInputVal.val());
+    }
+});
+
+//remove error on field focus
+$("#onlineDataModal .modal-content #join-room-form input").focus(function () { $("#onlineDataModal form .error-msg").removeClass("display") });
+
+
+
+//generate room code
+async function generateRoomCode() {
+
+    codeGenerateBtn.addClass("generating");
+
+    try {
+        const docref = await addDoc(collection(db, "rooms"), {
+
+        });
+
+        roomCode = docref.id.slice(0, 8);
+        codeGenerateBtn.removeClass("generating");
+        codeInputVal.val(roomCode);
+
+
+    } catch (e) {
+        alert("following error occured: " + e);
+        codeGenerateBtn.removeClass("generating");
+    }
+
+}
+
+
+//player joins the room
+async function enterPlayer() {
+
+    var flag = false;
+    const querySnap = await getDocs(collection(db, 'rooms'));
+
+    playBtn.addClass("playing");
+
+    try {
+        querySnap.forEach((docu) => {
+            if (docu.id.slice(0, 8) == roomCode) {
+                fullRoomID = docu.id;
+                addPlayer();
+                flag = true;
+            }
+        });
+
+
+    } catch (e) {
+        formErrorMsg.text(e);
+        formErrorMsg.addClass("display");
+        flag = true;
+        playBtn.removeClass("playing");
+    }
+
+    if (!flag) {
+        formErrorMsg.text("room does not exist");
+        formErrorMsg.addClass("display");
+        playBtn.removeClass("playing");
+
+    }
+
+}
+
+
+//Add Player to room
+async function addPlayer() {
+
+    const snapshot = await getCountFromServer(collection(db, "rooms", fullRoomID, "players"));
+    const playerCount = snapshot.data().count;
+    console.log("player count is: " + playerCount);
+    if (playerCount < 2) {
+
+        try {
+            const docref = await addDoc(collection(db, "rooms", fullRoomID, "players"), {
+                name: pName,
+                score: 0,
+                currentScore: 0,
+                roomCode: roomCode,
+                dice: 1,
+                won: 0,
+                turn: playerCount == 0 ? 1 : 0,
+                // roomCreator:playerCount==0
+                timeStamp: new Date().getTime(),
+            });
+
+            fullDataID = docref.id;
+            // writeName(fullRoomID);
+            playerName.text(pName);
+            codePlate.text(roomCode);
+            $(".room-details").addClass("online");
+            $(".player-dashboard .play-btns").removeClass("hide");
+            $(".player-dashboard .player--1 .play-btns").addClass("hide");
+            $(".player-dashboard .player--0 .play-btns").addClass("op-not");
+
+            updateDashboard(true);
+            playBtn.removeClass("playing");
+            $("#onlineDataModal .modal-header button").click();
+            $("div.curtain").addClass("open");
+
+
+        } catch (e) {
+            console.log(e)
+            formErrorMsg.text("Unable to join. Some error occured");
+            formErrorMsg.addClass("display");
+            playBtn.removeClass("playing");
+        }
+
+        if (playerCount == 1) {
+            $(".info-modal .btns-container").addClass("disabled");
+        }
+
+        else {
+            $(".info-modal .btns-container").removeClass("disabled");
+        }
+
+    }
+
+    else {
+        formErrorMsg.text("Room is full");
+        formErrorMsg.addClass("display");
+        playBtn.removeClass("playing");
+    }
+
+}
+
+//Update score
+async function updateScoreLive(score) {
+
+    const ref = doc(db, "rooms", fullRoomID, "players", fullDataID);
+    const data = updateDoc(ref, {
+        score: score,
+    })
+}
+
+//Update current score
+async function updateCurrentScoreLive(score) {
+
+    // console.log("updating to: "+ score);
+    const ref = doc(db, "rooms", fullRoomID, "players", fullDataID);
+    const data = updateDoc(ref, {
+        currentScore: score,
+    })
+}
+
+//Dice roll update
+async function diceRollLive(num) {
+
+    const refo = doc(db, "rooms", fullRoomID, "players", fullOpDataID);
+    const datao = updateDoc(refo, {
+        dice: num,
+    })
+
+    const refp = doc(db, "rooms", fullRoomID, "players", fullDataID);
+    const datap = updateDoc(refp, {
+        dice: num,
+    })
+
+
+}
+
+//Update turn
+async function updateTurn(num) {
+
+    const refo = doc(db, "rooms", fullRoomID, "players", fullOpDataID);
+    const datao = updateDoc(refo, {
+        turn: num == 0 ? 1 : 0,
+    })
+
+    const refp = doc(db, "rooms", fullRoomID, "players", fullDataID);
+    const datap = updateDoc(refp, {
+        turn: num == 0 ? 0 : 1,
+    })
+
+}
+
+//Declare Winner
+async function declareWinner() {
+    const ref = doc(db, "rooms", fullRoomID, "players", fullDataID);
+    const data = updateDoc(ref, {
+        won: 1,
+    });
+}
+
+//New Game
+async function restartGame() {
+
+    const ref = doc(db, "rooms", fullRoomID, "players", fullDataID);
+    const data = updateDoc(ref, {
+        score: 0,
+        dice: 1,
+        currentScore: 0,
+    });
+
+    const refo = doc(db, "rooms", fullRoomID, "players", fullOpDataID);
+    const datao = updateDoc(refo, {
+        score: 0,
+        dice: 1,
+        currentScore: 0,
+    });
+}
+
+//End Game
+async function endGame() {
+    const ref = doc(db, "rooms", fullRoomID, "players", fullDataID);
+    const data = updateDoc(ref, {
+        currentScore: 0,
+        score: 0,
+        name: pName + " Left the room",
+    });
+}
+
+//Update Dashboard (enter false to break the connection)
+async function updateDashboard(flag) {
+
+    const player = onSnapshot(doc(db, "rooms", fullRoomID, "players", fullDataID), (doc) => {
+        console.log("Current data: ", doc.data().score);
+        playerCurrentScore.text(doc.data().currentScore);
+        playerScore.text(doc.data().score);
+
+        //update player turn
+        if (doc.data().turn == 0) {
+            $(".player-dashboard .player--0 .play-btns").addClass("hide");
+            $(".player--0").removeClass("player--active");
+            $(".player--1").addClass("player--active");
+        }
+
+        else {
+            $(".player-dashboard .player--0 .play-btns").removeClass("hide");
+            $(".player--1").removeClass("player--active");
+            $(".player--0").addClass("player--active");
+        }
+
+        //update dice image
+        $("img.dice").attr("src", "assets/imgs/dice-" + doc.data().dice + ".png");
+
+        //check for winner
+        if (doc.data().won == 1) {
+            $(".player--0").addClass("player--winner");
+            $(".player--1").removeClass("player--active");
+            $(".play-btns").addClass("hide");
+        }
+    });
+
+    const opponentq = query(collection(db, "rooms", fullRoomID, "players"), where("name", "!=", pName));
+
+    const opp = onSnapshot(opponentq, (querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            fullOpDataID = doc.id;
+            opponentName.text(doc.data().name);
+            opponentScore.text(doc.data().score);
+            opponentCurrentScore.text(doc.data().currentScore);
+
+            //check for winner
+            if (doc.data().won == 1) {
+                $(".player--1").addClass("player--winner");
+                $(".play-btns").addClass("hide");
+            }
+
+            $(".player-dashboard .player--0 .play-btns").removeClass("op-not");
+
+        })
+    },
+        (error) => {
+            console.log(error);
+        });
+
+    if (!flag) {
+        player();
+        opp();
+
+    }
+
+}
+
+export { updateScoreLive, updateCurrentScoreLive, diceRollLive, updateTurn, declareWinner, updateDashboard, restartGame, endGame };
---
+++ assets/js/script.js
@@ -0,0 +1,179 @@
+'use strict';
+import { updateScoreLive,updateCurrentScoreLive,diceRollLive,updateTurn, declareWinner, updateDashboard, restartGame, endGame} from './backend.js';
+
+$(document).ready(function () {
+
+   var isOnline = true;
+
+    //play with friend button
+    $("#play-friend-btn").click(function () {
+        $("div.curtain").addClass("open");
+        $(".room-details").removeClass("online");
+        $(".player-dashboard .play-btns").removeClass("hide");
+        $(".info-modal .btns-container").removeClass("disabled");
+        isOnline = false;
+    });
+
+    //dice roll button
+    $("button.btn--roll").click(function () {
+        diceRoll();
+    });
+
+   //dice hold button
+    $("button.btn--hold").click(function () {
+
+        let currentScore = $(".player--active .current-score").text();
+        UpdatePlayerScore(currentScore);
+        updateCurrentScore(1);
+    });
+
+    //new game button
+    $("button.btn-new-game").click(function () {
+       
+        $("button.btn--menu").click();
+        if(isOnline){
+            restartGame();
+        }
+            renewGame();   
+    });
+
+    //dashboard menu button
+    $("button.btn--menu").click(function(){
+       $(".player-dashboard").toggleClass("show-details");
+       $(this).toggleClass("cross");
+
+    });
+
+    //close game button
+    $("button.btn-end").click(function(){
+        renewGame();
+        $("button.btn--menu").click();
+        $("div.curtain").removeClass("open");
+        $("#PlayerName").text("Player 1");
+        $("#OpponentName").text("Player 2");
+
+        if(isOnline){
+            updateDashboard(false);
+            endGame();
+        }
+
+    });
+
+
+   //roll dice function
+    function diceRoll() {
+
+        let x = 10;
+
+        while (x > 6){
+            x = Math.floor(Math.random() * (6 - 1)) + 1;
+        }
+
+        if(isOnline){
+             diceRollLive(x);
+        }
+
+        $("img.dice").attr("src", "assets/imgs/dice-" + x + ".png");
+        updateCurrentScore(x);
+    }
+
+
+    //update current score
+    function updateCurrentScore(score) {
+        let finalScore = $(".player--active .current-score").text();
+        finalScore = Math.floor(finalScore) + score;
+
+        if (score == 1) {
+
+            checkWinner();
+            $(".player--active .current-score").text(0);
+            switchPlayer();
+            if(isOnline){
+                updateCurrentScoreLive(0);
+            }
+        }
+
+        else {
+            $(".player--active .current-score").text(finalScore);
+            if(isOnline){
+                updateCurrentScoreLive(finalScore);
+            }
+        }
+
+    }
+
+
+   //switch player 
+    function switchPlayer() {
+
+        
+        if(isOnline){
+            if($(".player.player--0").hasClass("player--active")){
+                updateTurn(0);
+            }
+            else{
+                updateTurn(1);
+            }
+        }
+
+        else{
+            $(".player").toggleClass("player--active");
+        }
+    }
+
+
+   //update final score
+    function UpdatePlayerScore(score) {
+        let finalScore = $(".player--active .score").text();
+        finalScore = parseInt(finalScore, 10) + parseInt(score, 10);
+       // console.log("final score is" + finalScore);
+
+        $(".player--active .score").text(finalScore);
+
+        if(isOnline){
+            updateScoreLive(finalScore);
+        }
+
+
+    }
+
+    //check for winner
+    function checkWinner() {
+        console.log("checking winner");
+
+        let finalScore = $(".player--active .score").text();
+        finalScore = parseInt(finalScore, 10);
+
+        if (finalScore >= 20) {
+            $(".player--active").addClass("player--winner");
+            console.log("winner is found");
+            enabledisablebtns(0);
+            if(isOnline){
+                declareWinner();
+            }
+        }
+    }
+
+    //disable buttons
+    function enabledisablebtns(val) {
+
+        if (val == 0) {
+            $("button.btn--roll").addClass("hidden");
+            $("button.btn--hold").addClass("hidden");
+        }
+
+        else if (val == 1) {
+            $("button.btn--roll").removeClass("hidden");          
+            $("button.btn--hold").removeClass("hidden");
+        }
+    }
+
+    //restart game
+    function renewGame(){
+        enabledisablebtns(1);
+        updateCurrentScore(1);
+        $(".player").removeClass("player--winner");
+        $(".player .score").text(0);
+    }
+
+});
